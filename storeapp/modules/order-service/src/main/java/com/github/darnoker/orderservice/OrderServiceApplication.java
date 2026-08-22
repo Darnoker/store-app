34a4ec2086942c29cab4699e8d48ed37fde4c6cf
Darnok <Darnok10000@gmail.com>
2026-08-22 13:26:47 +0200

    Add order-service

--- storeapp/modules/order-service/src/main/java/com/github/darnoker/orderservice/OrderServiceApplication.java
+++ storeapp/modules/order-service/src/main/java/com/github/darnoker/orderservice/OrderServiceApplication.java
@@ -1,13 +1,13 @@
-package com.github.darnoker.storeapp;
+package com.github.darnoker.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StoreappApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreappApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
 }

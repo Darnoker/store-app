85890ab8b899e1aedf8d99ed430bae6ac1836082
Darnok <Darnok10000@gmail.com>
2026-08-25 23:49:30 +0200

    add admin authority

--- modules/product-service/src/test/java/com/github/darnoker/productservice/api/ProductApiIntegrationTest.java
+++ modules/product-service/src/test/java/com/github/darnoker/productservice/api/ProductApiIntegrationTest.java
@@ -2,7 +2,7 @@ package com.github.darnoker.productservice.api;
 
 import com.github.darnoker.productservice.generated.model.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
@@ -12,17 +12,18 @@ import java.util.Map;
 import static java.util.Objects.requireNonNull;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WithMockUser(roles = "ADMIN")
 class ProductApiIntegrationTest extends BaseApiTest {
 
     @Test
     void createsAndFindsBook() throws Exception {
         CreateProductRequest request = new CreateProductRequest().name("The Witcher").description("Fantasy book").price(new BigDecimal("49.99"))
                 .productType(ProductType.BOOK).details(Map.of("isbn", "978-83-0000-000-0", "pages", 320, "author", "Andrzej Sapkowski", "publisher", "Example Publisher", "language", "PL"));
-        MvcResult result = mvc.perform(post("/products").contentType("application/json").content(jsonMapper.writeValueAsString(request)))
+        MvcResult result = mvc.perform(post("/products").with(jwt().authorities(new SimpleGrantedAuthority("ROLE_ADMIN")))
+                        .contentType("application/json").content(jsonMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated()).andExpect(header().string("Location", containsString("/products/"))).andReturn();
         ProductDTO created = jsonMapper.readValue(result.getResponse().getContentAsString(), ProductDTO.class);
         assertEquals("The Witcher", created.getName());
@@ -36,7 +37,9 @@ class ProductApiIntegrationTest extends BaseApiTest {
     void listsProductsFilteredByType() throws Exception {
         CreateProductRequest request = new CreateProductRequest().name("Steel Longsword").description("Two handed sword").price(new BigDecimal("299.99"))
                 .productType(ProductType.SWORD).details(Map.of("damage", 42, "weight", 3.5, "length", 115.0, "material", "STEEL"));
-        mvc.perform(post("/products").contentType("application/json").content(jsonMapper.writeValueAsString(request))).andExpect(status().isCreated());
+        mvc.perform(post("/products").with(jwt().authorities(new SimpleGrantedAuthority("ROLE_ADMIN")))
+                        .contentType("application/json").content(jsonMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
         mvc.perform(get("/products").queryParam("type", "SWORD")).andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].productType").value("SWORD"));
     }
@@ -47,9 +50,11 @@ class ProductApiIntegrationTest extends BaseApiTest {
                 .productType(ProductType.BOOK).details(Map.of("isbn", "978-83-0000-000-0", "pages", 320, "author", "Andrzej Sapkowski", "publisher", "Example Publisher", "language", "PL"));
         CreateProductRequest secondRequest = new CreateProductRequest().name("Steel Longsword").description("Two handed sword").price(new BigDecimal("299.99"))
                 .productType(ProductType.SWORD).details(Map.of("damage", 42, "weight", 3.5, "length", 115.0, "material", "STEEL"));
-        ProductDTO first = jsonMapper.readValue(mvc.perform(post("/products").contentType("application/json").content(jsonMapper.writeValueAsString(firstRequest)))
+        ProductDTO first = jsonMapper.readValue(mvc.perform(post("/products").with(jwt().authorities(new SimpleGrantedAuthority("ROLE_ADMIN")))
+                        .contentType("application/json").content(jsonMapper.writeValueAsString(firstRequest)))
                 .andExpect(status().isCreated()).andReturn().getResponse().getContentAsString(), ProductDTO.class);
-        ProductDTO second = jsonMapper.readValue(mvc.perform(post("/products").contentType("application/json").content(jsonMapper.writeValueAsString(secondRequest)))
+        ProductDTO second = jsonMapper.readValue(mvc.perform(post("/products").with(jwt().authorities(new SimpleGrantedAuthority("ROLE_ADMIN")))
+                        .contentType("application/json").content(jsonMapper.writeValueAsString(secondRequest)))
                 .andExpect(status().isCreated()).andReturn().getResponse().getContentAsString(), ProductDTO.class);
 
         mvc.perform(get("/products").queryParam("ids", first.getId().toString(), second.getId().toString()))
